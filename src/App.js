6104325e3eb3191f29b31227a067820440cc0c73
SunInvictus <wotgibson@yandex.ru>
2023-12-12 22:46:03 +0300

    Fix error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const greeting = "Кармазин Е. Д.";
 
-  console.log('This is App ');
+
   return (
     <div className="App">
      <header className="App-header">
